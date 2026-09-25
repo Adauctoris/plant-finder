d3f26a540f2a1c33eb704071f6670ac1c9d4b74b
Adauctoris <261737071+Adauctoris@users.noreply.github.com>
2026-09-25 22:15:22 +0100

    Cleaned up, and fixed a spelling error in comments.
    Spelling error within code comments resolved

--- plant_finder_app.py
+++ plant_finder_app.py
@@ -1,16 +1,15 @@
-import streamlit as st
-import sqlite3
-import numpy as np
 import pandas as pd
-from plant_finder_main import execute_request, data_input
+import streamlit as st
+
+from plant_finder_main import data_input
 
 st.set_page_config(layout="wide")
 
-def st_normal():
+def st_normal(): #Normal page width option
     _, col, _ = st.columns([1, 2, 1])
     return col
 
-def st_wider():
+def st_wider(): #Wider page width option
     _, col, _ = st.columns([1, 5, 1])
     return col
 
@@ -19,7 +18,7 @@ with st_normal():
     st.title("Plant Finder App")
 
 
-    option = st.selectbox(
+    option = st.selectbox( #Primary filter search
         'Please select an option:',
         [ 'Soil Search', 'pH Search'])
 
@@ -27,18 +26,18 @@ with st_normal():
 
 if option == 'Soil Search':
     with st_normal():
-        soil_type = st.selectbox(
+        soil_type = st.selectbox( #Soil types to select
             'Please select a soil type:',
             ['Gravel', 'Clay', 'Sand'])
         st.write('You selected: ', soil_type)
 
-    results = data_input('soil', soil_type, None, None)
+    results = data_input('soil', soil_type, None, None) #Sends request to backend
     df = pd.DataFrame(results, columns=['ID', 'Name', 'Description', 'pH Min', 'pH Max', 'Sunlight', 'Water Frequency'])
-    with st_wider():
+    with st_wider(): #Receives and proceeds to write the returned plants
         st.write(df)
 
 else:
-    with st_normal():
+    with st_normal(): #pH range options
         ph_min = st.slider('Select minimum pH value:', 
                            0, 14, 6)
         ph_max = st.slider('Select maximum pH value:', 
@@ -47,5 +46,5 @@ else:
 
     results = data_input('ph', None, ph_min, ph_max)
     df = pd.DataFrame(results, columns=['ID', 'Name', 'Description', 'pH Min', 'pH Max', 'Sunlight', 'Water Frequency'])
-    with st_wider():
+    with st_wider(): #Receives and proceeds to write the returned plants
         st.write(df)
